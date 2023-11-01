ebf24b063fdccc049d6200b87ff6cc4c00e521a8
Weslley Barros <weslley17w@gmail.com>
2023-11-01 14:40:13 -0300

    chore: Adiciona rota /user

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import homeRoutes from "./homeRouters"
+import userRoutes from "./userRouters"
 
 const router = Router()
 
 router.use("/", homeRoutes)
+router.use("/user", userRoutes)
 
 export { router }
---
+++ backend/src/routes/userRouters.ts
@@ -0,0 +1,5 @@
+import { Router } from "express"
+
+const userRouters = Router()
+
+export default userRouters
