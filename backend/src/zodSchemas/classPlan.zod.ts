a01356cbba64a11ae1e44560efca2a7229aa53e5
Weslley Barros <weslley17w@gmail.com>
2023-11-25 16:57:39 -0300

    chore: Corige erro no campo ClassPlan.userId

--- backend/src/zodSchemas/classPlan.zod.ts
+++ backend/src/zodSchemas/classPlan.zod.ts
@@ -1,13 +1,12 @@
-// import { z } from "zod"
+import { z } from "zod"
 
-// // prettier-ignore
-// const classPlanSchema = z.object({
-//   title: z.string().min(5, "Título deve ter no mínimo 5 caracteres"),
-//   goals: z.string().max(600, "Objetivos deve ter no máximo 600 caracteres"),
-//   observations: z
-//     .string()
-//     .max(500, "Observações deve ter no máximo 500 caracteres"),
-//   userId: z.string(),
-// })
+const classPlanSchema = z.object({
+  title: z.string().min(5, "Título deve ter no mínimo 5 caracteres"),
+  goals: z.string().max(600, "Objetivos deve ter no máximo 600 caracteres"),
+  observations: z
+    .string()
+    .max(500, "Observações deve ter no máximo 500 caracteres"),
+  userId: z.string().uuid("Id do usuário inválido"),
+})
 
-// export { classPlanSchema }
+export { classPlanSchema }
