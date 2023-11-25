8c84ec48bee3ad40147157b20481989750887ab1
Weslley Barros <weslley17w@gmail.com>
2023-11-25 17:13:14 -0300

    chore: Tipa as saida do create do cprisma

--- backend/src/controllers/classPlanController.ts
+++ backend/src/controllers/classPlanController.ts
@@ -3,20 +3,28 @@ import { prisma } from "@/prisma"
 import { ZodError } from "zod"
 import { classPlanSchema } from "@/zodSchemas/classPlan.zod"
 import { fromZodError } from "zod-validation-error"
+import { Prisma } from "@prisma/client"
 
 export default class classPlanController {
   create = async (req: Request, res: Response) => {
     try {
       const data = classPlanSchema.parse(req.body)
-      await prisma.classPlan.create({
+      const classPlan = (await prisma.classPlan.create({
         data: {
           ...data,
         },
         include: {
-          user: true,
+          user: {
+            select: {
+              id: true,
+              email: true,
+              name: true,
+            },
+          },
         },
-      })
-      res.status(201).json(data)
+      })) as Prisma.ClassPlanCreateInput
+
+      res.status(201).json(classPlan)
     } catch (err) {
       if (err instanceof ZodError) {
         res.status(400).json(fromZodError(err))
